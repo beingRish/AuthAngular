6527d70cee5709bc46754d7ce3e54a4622903b85
Rishabh Kumar <rishabhjuly11@gmail.com>
2024-07-27 19:03:51 +0530

    updated

--- src/app/emp/emp.component.ts
+++ src/app/emp/emp.component.ts
@@ -8,52 +8,65 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   templateUrl: './emp.component.html',
   styleUrls: ['./emp.component.css']
 })
-export class EmpComponent implements OnInit{
-  
+export class EmpComponent implements OnInit {
+
   user: any;
   userId: any;
   editMode!: boolean;
-  EditEmployeeForm!: FormGroup
+  EditEmployeeForm!: FormGroup;
+  EmployeeInfo: any;
 
   constructor(
     private _du: DesignUtilityService,
     private router: Router,
     private activatedRoute: ActivatedRoute,
     private fb: FormBuilder
-  ){}
+  ) { }
 
   ngOnInit(): void {
-    
+
     this.EditEmployeeForm = this.fb.group({
       name: ['', Validators.required],
       designation: ['', Validators.required],
       dept: ['', Validators.required],
       status: ['', Validators.required]
-  })
-
+    })
 
     this.userId = this.activatedRoute.snapshot.paramMap.get('id')
-    this._du.fetchSingleEmployee(this.userId).subscribe(res => {
-      this.user = res
-    })
 
     this.activatedRoute.queryParamMap.subscribe(res => {
       let qParams = res.get('EditMode');
-
       if (qParams != null) {
         this.editMode = true;
       } else {
         this.editMode = false;
       }
     })
+    this.getEmployee(this.userId);
   }
 
-  
-  onSubmit() {
-    console.log(this.EditEmployeeForm.value);
-    
+  getEmployee(userId: any) {
+    this._du.fetchSingleEmployee(userId).subscribe(res => {
+      this.user = res
+      console.log(res);
+    })
   }
-  
+
+
+  onSubmit() {
+    if (this.EditEmployeeForm.valid) {
+      const updatedEmployee = {
+        ...this.EditEmployeeForm.value,
+        id: new Date().getTime().toString()
+      };
+      this._du.updateEmployee(this.userId, updatedEmployee).subscribe(
+        (res: any) => {
+          this.onDiscard()
+        },
+      );
+    }
+  }
+
   onDiscard() {
     this.router.navigate([], { queryParams: { EditMode: null } })
   }
